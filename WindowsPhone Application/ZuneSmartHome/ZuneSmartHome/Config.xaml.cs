cdc87d1d1842a59de4511646b18159c9d036cc54
Luca Lacerda <wakesolutions@gmail.com>
2014-05-08 16:01:04 -0300

    Re-Upload
    Problemas e mais problemas kkkk

---
+++ WindowsPhone Application/ZuneSmartHome/ZuneSmartHome/Config.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using ZuneSmartHome.Resources;
+using Windows.Networking.Sockets;
+using Windows.Networking.Proximity;
+using System.Diagnostics;
+using Windows.Storage.Streams;
+using System.Threading.Tasks;
+using BluetoothConnectionManager;
+using System.Windows.Media;
+
+namespace ZuneSmartHome
+{
+    public partial class Config : PhoneApplicationPage
+    {
+        private ConnectionManager connectionManager;
+
+        public Config()
+        {
+            InitializeComponent();
+            connectionManager = new ConnectionManager();
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            connectionManager.Initialize();
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            connectionManager.Terminate();
+        }
+
+        private void ConnectAppToDeviceButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            AppToDevice();
+        }
+
+        private async void AppToDevice()
+        {
+            ConnectAppToDeviceButton.Content = "Conectando...";
+            PeerFinder.AlternateIdentities["Bluetooth:Paired"] = "";
+            var pairedDevices = await PeerFinder.FindAllPeersAsync();
+
+            if (pairedDevices.Count == 0)
+            {
+                Debug.WriteLine("Nenhum dispositivo pareado encontrado");
+            }
+            else
+            {
+                foreach (var pairedDevice in pairedDevices)
+                {
+                    if (pairedDevice.DisplayName == DeviceName.Text)
+                    {
+                        connectionManager.Connect(pairedDevice.HostName);
+                        ConnectAppToDeviceButton.Content = "Conectado";
+                        DeviceName.IsReadOnly = true;
+                        ConnectAppToDeviceButton.IsEnabled = false;
+                        continue;
+                    }
+                }
+            }
+        }
+
+        private void DeviceName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
