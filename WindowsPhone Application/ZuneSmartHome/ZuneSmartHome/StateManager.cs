88b081f20b85dafe32a740f9f3abe6c5c105b2dc
Luca Lacerda <wakesolutions@gmail.com>
2014-05-08 15:08:30 -0300

    Arquivos
    Todos os arquivos do projeto....

---
+++ WindowsPhone Application/ZuneSmartHome/ZuneSmartHome/StateManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZuneSmartHome
+{
+
+    /// <summary>
+    /// Class to control the state of the Arduino
+    /// </summary>
+    class StateManager
+    {
+        /// <summary>
+        /// The red led state 
+        /// </summary>
+        public bool Quarto { get; set; }
+
+        /// <summary>
+        /// The green led state 
+        /// </summary>
+        public bool Sala { get; set; }
+
+        /// <summary>
+        /// Initialize the state manager.
+        /// </summary>
+        public void Initialize()
+        {
+            Quarto = false;
+            Sala = false;
+        }
+    }
+}
